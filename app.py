d7ce3dbcacc3d2072ade29293372d0c8ec017dcc
Andy Evans <andrewevans0@outlook.com>
2017-02-15 16:50:42 +0000

    flask app and routing set up

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from flask import Flask
 app = Flask(__name__)
 
-print "hello world!"
+@app.route("/")
+def hello():
+    return "Hello World!"
 
-##creating the test app route
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=80, debug=True)
