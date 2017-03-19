9156dac85287b900dba9ee54ef9d7e7a7e883f1f
Andy Evans <andrewevans0@outlook.com>
2017-03-19 15:10:46 +0000

    testing motor moving

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask
 import RPi.GPIO as GPIO
-import time
+from time import sleep
 
 ###callout ip addresses
 ### pi zero 192.168.1.67
@@ -10,14 +10,34 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setwarnings(False)
-    GPIO.setup(18,GPIO.OUT)
-    print "LED on"
-    GPIO.output(18,GPIO.HIGH)
-    time.sleep(1)
-    print "LED off"
-    GPIO.output(18,GPIO.LOW)
+    GPIO.setmode(GPIO.BOARD)
+
+    Motor1A = 16
+    Motor1B = 18
+    Motor1E = 22
+    
+    GPIO.setup(Motor1A,GPIO.OUT)
+    GPIO.setup(Motor1B,GPIO.OUT)
+    GPIO.setup(Motor1E,GPIO.OUT)
+    
+    print "Going forwards"
+    GPIO.output(Motor1A,GPIO.HIGH)
+    GPIO.output(Motor1B,GPIO.LOW)
+    GPIO.output(Motor1E,GPIO.HIGH)
+    
+    sleep(2)
+    
+    print "Going backwards"
+    GPIO.output(Motor1A,GPIO.LOW)
+    GPIO.output(Motor1B,GPIO.HIGH)
+    GPIO.output(Motor1E,GPIO.HIGH)
+    
+    sleep(2)
+    
+    print "Now stop"
+    GPIO.output(Motor1E,GPIO.LOW)
+    
+    GPIO.cleanup()
     return "Hello World!"
 
 if __name__ == "__main__":
